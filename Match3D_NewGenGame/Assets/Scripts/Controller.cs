3027a5be858e90e70788a2d5f42869c6cdde8b30
Orkun Hacılar <orkun.hacilar@gmail.com>
2024-01-29 17:36:47 +0300

    Update

--- Match3D_NewGenGame/Assets/Scripts/Controller.cs
+++ Match3D_NewGenGame/Assets/Scripts/Controller.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Linq;
 using UnityEngine;
 
 public class Controller : MonoBehaviour
@@ -7,9 +9,10 @@ public class Controller : MonoBehaviour
     [SerializeField] GameObject[] ControllerPositions;
     private bool isGameObjectOneEmpty = true;
     private bool isGameObjectTwoEmpty = true;
-    private bool shouldTrigger = true;
     [SerializeField] float lerpSpeed = 5f;
     private BoxCollider boxCollider;
+    private List<GameObject> addedFood = new List<GameObject>();
+    Rigidbody foodRB;
 
     Food food;
 
@@ -30,36 +33,98 @@ public class Controller : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter(Collider other)
+   
+
+    private void OnTriggerStay(Collider other)
     {
-        if (shouldTrigger)
+
+        if (other != null)  
         {
-            // food.isItOkForTrigger = false;
-            
+             
             if (isGameObjectOneEmpty)
             {
+                food.isItOkForTrigger = false; // suruklemeyi kapat
                 other.transform.position = Vector3.Lerp(other.transform.position, ControllerPositions[0].transform.position, Time.deltaTime *  lerpSpeed);
-                isGameObjectOneEmpty = false;
+
+                //obje sabitlenmeli ki yeri degismesin
+                foodRB = other.gameObject.GetComponent<Rigidbody>();
+                foodRB.constraints = RigidbodyConstraints.FreezePositionX;
+                foodRB.constraints = RigidbodyConstraints.FreezePositionZ;
+
+                addedFood.Add(other.gameObject); // obje eklendi
+                food.isItOkForTrigger = true; // suruklemeyi ac
+
+                //objeyi tutmam lazim bekletmem lazim yok etmek icin
+                isGameObjectOneEmpty = false; //Konum 1 doldu.
             }
             else if(isGameObjectTwoEmpty)
             {
+                food.isItOkForTrigger = false; // suruklemeyi kapat
                 other.transform.position = Vector3.Lerp(other.transform.position, ControllerPositions[1].transform.position, Time.deltaTime * lerpSpeed);
-                isGameObjectTwoEmpty = false;
+
+                //obje sabitlenmeli ki yeri degismesin
+                foodRB = other.gameObject.GetComponent<Rigidbody>();
+                foodRB.constraints = RigidbodyConstraints.FreezePositionX;
+                foodRB.constraints = RigidbodyConstraints.FreezePositionZ;
+
+                food.isItOkForTrigger = true; // suruklemeyi ac
+                addedFood.Add(other.gameObject); // obje eklendi
+                //objeyi tutmam lazim bekletmem lazim yok etmek icin
+                isGameObjectTwoEmpty = false; //Konum 2 doldu.
             }
 
-            if(isGameObjectOneEmpty == false && isGameObjectOneEmpty == false)
+            if(isGameObjectOneEmpty == false && isGameObjectOneEmpty == false) // IKI ALANDA DOLDU ISE.
             {
+                //iki objeyide yok et iki yeride online yap.
 
-                shouldTrigger = false;
-               //  food.isItOkForTrigger = true;
+                if (CheckTags())
+                {
+                    foreach (var item in addedFood)
+                    {
+                      Collider foodCollider =  item.gameObject.GetComponent<Collider>();
+                      foodCollider.isTrigger = true; //Asaya dusup sanki yok oluyomus efekti. Match3D'de oldugu gibi
+                    }
+
+                    ClearList();
+                    isGameObjectOneEmpty = true; //Konum 1 bosaldi.
+                    isGameObjectTwoEmpty = true; //Konum 2 bosaldi.
+                }
+
+                       
+                    
+                
+
+                
+                
+
+               
             }
-            else
-            {
-                shouldTrigger = true;
-            }
+            
         }
 
        
+    }
+
+    bool CheckTags()
+    {
+        string firstTag = addedFood[0].tag; //ilk tagi aldim
+        for (int i = 1; i < addedFood.Count; i++)
+        {
+            // Eğer bir tag farklıysa, false döndür
+            if (addedFood[i].tag != firstTag)
+            {
+                return false;
+            }
+        }
+
+        // Tüm objelerin tag'leri aynıysa true döndür
+        return true;
+    }
+
+    void ClearList()
+    {
+        addedFood.Clear();
+        Debug.Log("listtemizlendi");
     }
 
 }
